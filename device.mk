c12c1c88cd00ff164f1264e9a2b306a61f5069e0
Josh Chasky <jchasky@gmail.com>
2019-09-27 12:55:41 -0400

    Ensure the proper Google Assistant prop is inherited
    * Also add ANGLE prop
    
    Change-Id: I221e6bec77a5e8e1c0ea517666fbe2cb111eb64f
    Signed-off-by: RJ Trujillo <certifiedblyndguy@gmail.com>

--- device.mk
+++ device.mk
@@ -723,4 +723,9 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/fstab.postinstall:$(TARGET_COPY_OUT_PRODUCT)/etc/fstab.postinstall
 
+# Enable missing props
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.opa.eligible_device=true \
+    ro.gfx.angle.supported=true
+
 -include device/google/wahoo/device-carbon.mk
